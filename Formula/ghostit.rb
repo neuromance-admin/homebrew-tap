2d495d426e27a342e4f1d61301bd181789f40067
neuromance-admin <halicon@gmail.com>
2026-04-11 16:41:40 +0000

    Update GhostIT to v0.1.2

--- Formula/ghostit.rb
+++ Formula/ghostit.rb
@@ -1,26 +1,26 @@
 class Ghostit < Formula
   desc "Encrypt any folder on your drive. No account. No server. Your key, your data."
   homepage "https://github.com/neuromance-admin/ghostit"
-  version "0.1.1"
+  version "0.1.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.1/ghostit-macos-arm.tar.gz"
-      sha256 "f8eef5fbe230d559f3100a0d68b3b1f1d3475fbb6b55b0baa55e60e094100a00"
+      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.2/ghostit-macos-arm.tar.gz"
+      sha256 "b2c20fecdb7f08316c3c86aa75af20fee7c7804c81714d722c39eee80c272387"
     else
-      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.1/ghostit-macos-intel.tar.gz"
-      sha256 "e6bdb0d095ca512683eea01052fbe440ec27689b6b690f8cd0ba4587f83346b6"
+      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.2/ghostit-macos-intel.tar.gz"
+      sha256 "b22db93fa9942bea42d5a945bcda7bfd21ba22b5e65de553baaf5a0ee96ac1a7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.1/ghostit-linux-arm64.tar.gz"
-      sha256 "8f4f4e217a38cb063d264bf824e364d4448ec3136d3f779c686d18697ae3a270"
+      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.2/ghostit-linux-arm64.tar.gz"
+      sha256 "08ecb32ea52bb1a8cbbafb6ebe3ac1e708028516ae9dd1bf972d94d2ebaf02ca"
     else
-      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.1/ghostit-linux-x86_64.tar.gz"
-      sha256 "6f6f9a21b62a449850e1b425d03d1a46aa627d644e5234ed2e6b00aed7ed7c88"
+      url "https://github.com/neuromance-admin/ghostit/releases/download/v0.1.2/ghostit-linux-x86_64.tar.gz"
+      sha256 "7d7101570279240386fcde17b603f9c7ebd63d4d911206ef0708a6acf5f655c2"
     end
   end
 
